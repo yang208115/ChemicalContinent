0ece2974ed744175a747ae87044abc05beffd7b4
luoyunfa-250 <a3305587173@outlook.com>
2023-10-29 12:32:02 +0800

    balloon

--- balloon/mian.py
+++ balloon/mian.py
@@ -6,7 +6,7 @@ import sys
 pygame.init()
 
 
-class ChemicalReaction():
+class Balloons():
     def __init__(self):
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption('Balloon')
@@ -14,8 +14,10 @@ class ChemicalReaction():
         self.balloon_number = 0
         self.topic_show = False
         self.topic_id = 0
+        self.score = 0
 
         self.game_run = True
+        self.if_game_over = False
 
     def run_game(self):
 
@@ -25,17 +27,25 @@ class ChemicalReaction():
             self._check_event()
             self.load_image()
             self.create_balloon()
-            self._update_balloon()
+            self.update_balloon()
+            self.set_show_score()
+            self.game_over()
 
             self.update_screen()
             pygame.display.update()
             self.clock.tick(60)
 
     def update_screen(self):
-        self.screen.fill((135, 180, 255))
-        self.screen.blit(self.bg, (0, 0))
-        self.balloons.draw(self.screen)
-        self.create_answer()
+        if not self.if_game_over:
+            self.screen.fill((135, 180, 255))
+            self.screen.blit(self.bg, (0, 0))
+            self.balloons.draw(self.screen)
+            self.create_answer()
+            self.screen.blit(self.score_text, (40, 20))
+
+        if self.if_game_over:
+            self.screen.blit(self.game_over_img, (200, 100))
+            self.screen.blit(self.start_game, (500, 380))
 
     def _check_event(self):
         for event in pygame.event.get():
@@ -51,9 +61,14 @@ class ChemicalReaction():
         self.bg = pygame.transform.scale(self.bg_tmp, (800, 600))
         self.balloon_tmp = pygame.image.load('image/balloon.png')
         self.balloon = pygame.transform.rotozoom(self.balloon_tmp, 0, 0.1)  # 984*1420
+        self.game_over_img_tmp = pygame.image.load('image/game_over.png')
+        self.game_over_img = pygame.transform.scale(self.game_over_img_tmp, (400, 200))
+        self.start_game = pygame.image.load('image/start_game.png')
         self.topic = [
             pygame.image.load('image/1_1.png'),  # 684*40
-            pygame.image.load('image/2_1.png')
+            pygame.image.load('image/2_1.png'),
+            pygame.image.load('image/3_1.png'),
+            pygame.image.load('image/4_1.png')
         ]
         self.answer_image = [
             pygame.image.load('image/1_2.png'),  # 234*190
@@ -68,7 +83,7 @@ class ChemicalReaction():
             a = 0
             b = random.sample([i for i in range(0, 4)], 4)
             for num in balloon_number_x:
-                Answer_id = random.randint(1, 4)
+                Answer_id = b[a]
                 self.answer_images = pygame.transform.rotozoom(self.answer_image[b[a]], 0, 0.1)
                 a += 1
                 balloons = Balloon(self.answer_images, self, Answer_id)
@@ -78,19 +93,26 @@ class ChemicalReaction():
 
     def create_answer(self):
         if self.topic_show == False:
-            self.topic_id = random.randint(0, 1)
+            self.topic_id = random.randint(0, 3)
         self.screen.blit(self.topic[self.topic_id], (60, 500))
         self.topic_show = True
 
+    def remove_balloon(self):
+        for balloon in self.balloons:
+            self.balloons.remove(balloon)
+            self.balloon_number = 0
+            self.topic_show = False
 
     def _check_balloon_y(self):
         for balloon in self.balloons:
             if balloon.rect.y >= 510:
-                self.balloons.remove(balloon)
-                self.balloon_number = 0
-                self.topic_show =  False
+                self.score -= 1
+                break
+        for balloon in self.balloons:
+            if balloon.rect.y >= 510:
+                self.remove_balloon()
 
-    def _update_balloon(self):
+    def update_balloon(self):
         self.balloons.update()
         self._check_balloon_y()
 
@@ -100,6 +122,25 @@ class ChemicalReaction():
                 if balloon.rect.x <= x <= balloon.rect.x + 190:
                     balloon.check_Answer()
 
+        if 500 <= x <= 500 + 200:
+            if 380 <= y <= 380 + 100:
+                self.remove_balloon()
+                self.create_balloon()
+                self.score = 0
+                self.if_game_over = False
+
+    def reset_balloon(self):
+        self.score += 1
+        self.remove_balloon()
+    def set_show_score(self):
+        self.score_text_tmp = " 得分：" + str(self.score)
+        self.font = pygame.font.Font("c:/windows/Fonts/simhei.ttf", 30)
+        self.score_text = self.font.render(self.score_text_tmp, True, (255, 255, 255))
+
+    def game_over(self):
+        if self.score < 0:
+            self.if_game_over = True
+
 
 class Balloon(pygame.sprite.Sprite):
     def __init__(self, image, balloon, Answer_id):
@@ -107,6 +148,7 @@ class Balloon(pygame.sprite.Sprite):
         self.answer_id = Answer_id
         self.image = image
         self.screen = balloon.screen
+        self.balloon = balloon
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = 10
@@ -116,9 +158,12 @@ class Balloon(pygame.sprite.Sprite):
         self.rect.y += self.speed
 
     def check_Answer(self):
-        print(self.answer_id)
-
+        if self.balloon.topic_id == self.answer_id:
+            self.balloon.reset_balloon()
+        elif self.balloon.topic_id != self.answer_id:
+            self.balloon.score -= 1
+            self.balloon.remove_balloon()
 
 if __name__ == '__main__':
-    cr = ChemicalReaction()
+    cr = Balloons()
     cr.run_game()
